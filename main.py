d4e6ff5801373f1dfebc5ec78d779a23600ff10c
Yumihiki <nibu10.majik@gmail.com>
2022-11-19 22:51:21 +0900

    フォントの位置がズレる問題の解消 中心の位置を取得し、その位置を元にcenter揃えになるようにすることで 問題解消を実施する

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     file_name = file_stem + file_suffix
     background_color = (0, 0, 0, 0)
     base_size = 256
+    center = base_size / 2
     image = Image.new(
         mode="RGBA",
         size=(base_size, base_size),
@@ -17,11 +18,13 @@ def main():
     image_draw = ImageDraw.Draw(im=image)
     image_font = None
     font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
-    split_size = base_size / len(text.splitlines())
+    split_size = int(
+        base_size / len(text.splitlines())
+    )
     count = 1
     for text in text.splitlines():
         size = None
-        font_size = 256
+        font_size = split_size
         while (size is None) or (size[2] > base_size) or (size[3] > base_size) \
                 and (font_size > 0):
             image_font = ImageFont.truetype(
@@ -31,16 +34,23 @@ def main():
             size = image_font.getbbox(text=text)
             font_size -= 1
         image_draw.text(
-            xy=(0, (split_size / 2) * count),
+            xy=(center, (split_size / 2) * count),
             text=text,
             fill="#000",
             font=image_font,
-            anchor="lm",
-            align="center",
+            anchor="mm",
         )
         count += 2
     image.save(fp=file_name)
 
 
 if __name__ == '__main__':
-    main()
+    input_texts = [
+        "hello\nworld",
+        "H",
+        "弓引",
+        "ゆみひき",
+        "Scrap\nBox"
+    ]
+    for input_text in input_texts:
+        main(input_text)
