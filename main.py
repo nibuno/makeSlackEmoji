c074ffd12045ab7114ad71f61e34bcb9ae28fbf8
Yumihiki <nibu10.majik@gmail.com>
2022-11-19 15:05:24 +0900

    出力ファイルの作成に伴う修正 ・フォントサイズの拡大 ・テキスト名を利用したファイル名の修正 ・背景画像の透明化

--- main.py
+++ main.py
@@ -3,36 +3,38 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def main():
-    file_name = "file" + ".png"
-    font_color = "#fff"
+    text = "Hello\nWorld"
+    file_stem = "_".join(text.splitlines())
+    file_suffix = ".png"
+    file_name = file_stem + file_suffix
+    background_color = (0, 0, 0, 0)
     base_size = 256
     image = Image.new(
-        mode="RGB",
+        mode="RGBA",
         size=(base_size, base_size),
-        color=font_color
+        color=background_color
     )
     image_draw = ImageDraw.Draw(im=image)
-    font = None
-    text = "Hello\nWorld"
+    image_font = None
     font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
     split_size = base_size / len(text.splitlines())
     count = 1
     for text in text.splitlines():
         size = None
-        font_size = 100
+        font_size = 256
         while (size is None) or (size[2] > base_size) or (size[3] > base_size) \
                 and (font_size > 0):
-            font = ImageFont.truetype(
+            image_font = ImageFont.truetype(
                 font=font_path,
                 size=font_size
             )
-            size = font.getbbox(text=text)
+            size = image_font.getbbox(text=text)
             font_size -= 1
         image_draw.text(
             xy=(0, (split_size / 2) * count),
             text=text,
             fill="#000",
-            font=font,
+            font=image_font,
             anchor="lm",
             align="center",
         )
