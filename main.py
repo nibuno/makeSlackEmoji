487faa61e04937c93dff7e912733bd772043d798
Yumihiki <nibu10.majik@gmail.com>
2022-11-23 16:04:54 +0900

    クラス化の対応

--- main.py
+++ main.py
@@ -2,137 +2,126 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-def main(text, font_color="#000000"):
-    file_stem = "_".join(text.splitlines())
-    file_suffix = ".png"
-    file_name = file_stem + file_suffix
-    background_color = (0, 0, 0, 0)
-    base_size = 128
-    center = base_size / 2
-    image = Image.new(
-        mode="RGBA",
-        size=(base_size, base_size),
-        color=background_color
-    )
-    image_draw = ImageDraw.Draw(im=image)
-    image_font = None
-    font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
-    split_size = int(
-        base_size / len(text.splitlines())
-    )
-    count = 1
-    right = 2
-    bottom = 3
-    for text in text.splitlines():
-        image_font = calc_font_size(
+class MakeSlackEmoji:
+    def __init__(self, text):
+        self.text = text
+        self.file_stem = "_".join(self.text.splitlines())
+        self.file_suffix = ".png"
+        self.file_name = self.file_stem + self.file_suffix
+        self.background_color = (0, 0, 0, 0)
+        self.font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
+
+    def main(self, font_color="#000000"):
+        base_size = 128
+        center = base_size / 2
+        image = Image.new(
+            mode="RGBA",
+            size=(base_size, base_size),
+            color=self.background_color
+        )
+        image_draw = ImageDraw.Draw(im=image)
+        split_size = int(
+            base_size / len(self.text.splitlines())
+        )
+        count = 1
+        right = 2
+        bottom = 3
+        for text in self.text.splitlines():
+            image_font = self._calc_font_size(
+                base_size,
+                bottom,
+                right,
+                split_size,
+                text
+            )[0]
+            image_draw.text(
+                xy=(center, (split_size / 2) * count),
+                text=text,
+                fill=font_color,
+                font=image_font,
+                anchor="mm",
+            )
+            count += 2
+        image.save(fp=self.file_name)
+
+    # todo: 重複ロジックの切り出し
+    def auto_font_size_change(self, font_color="#000000"):
+        base_size = 128 * 2
+        center = base_size / 2
+        right = 2
+        bottom = 3
+        bounding_bottoms = []
+        for text in self.text.splitlines():
+            bounding_box = self._calc_font_size(
+                base_size,
+                bottom,
+                right,
+                base_size,
+                text
+            )[1]
+            bounding_bottoms.append(bounding_box[bottom])
+        image = Image.new(
+            mode="RGBA",
+            size=(base_size, sum(bounding_bottoms)),
+            color=self.background_color
+        )
+        image_draw = ImageDraw.Draw(im=image)
+        for i, text in enumerate(self.text.splitlines(), start=1):
+            image_font = self._calc_font_size(
+                base_size,
+                bottom,
+                right,
+                base_size,
+                text
+            )[0]
+            image_draw.text(
+                xy=(center, self._calc_y_axis(bounding_bottoms, i)),
+                text=text,
+                fill=font_color,
+                font=image_font,
+                anchor="mm",
+            )
+        resize_base = 128
+        image = image.resize((resize_base, resize_base))
+        image.save(fp=self.file_name)
+
+
+    def _calc_font_size(
+            self,
             base_size,
             bottom,
-            font_path,
-            image_font,
             right,
-            split_size,
-            text
-        )[0]
-        image_draw.text(
-            xy=(center, (split_size / 2) * count),
-            text=text,
-            fill=font_color,
-            font=image_font,
-            anchor="mm",
-        )
-        count += 2
-    image.save(fp=file_name)
+            font_size,
+            text):
+        bounding_box = None
+        while (bounding_box is None) or \
+                (bounding_box[right] > base_size) or \
+                (bounding_box[bottom] > base_size) \
+                and (font_size > 0):
+            image_font = ImageFont.truetype(
+                font=self.font_path,
+                size=font_size
+            )
+            bounding_box = image_font.getbbox(text=text)
+            font_size -= 1
+        return image_font, bounding_box
 
 
-# todo: 重複ロジックの切り出し
-def auto_font_size_change(texts, font_color="#000000"):
-    file_stem = "_".join(texts.splitlines())
-    file_suffix = ".png"
-    file_name = file_stem + file_suffix
-    background_color = (0, 0, 0, 0)
-    base_size = 128 * 2
-    center = base_size / 2
-    image_font = None
-    font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
-    right = 2
-    bottom = 3
-    bounding_bottoms = []
-    for text in texts.splitlines():
-        bounding_box = calc_font_size(
-            base_size,
-            bottom,
-            font_path,
-            image_font,
-            right,
-            base_size,
-            text
-        )[1]
-        bounding_bottoms.append(bounding_box[bottom])
-    image = Image.new(
-        mode="RGBA",
-        size=(base_size, sum(bounding_bottoms)),
-        color=background_color
-    )
-    image_draw = ImageDraw.Draw(im=image)
-    for i, text in enumerate(texts.splitlines(), start=1):
-        image_font = calc_font_size(
-            base_size,
-            bottom,
-            font_path,
-            image_font,
-            right,
-            base_size,
-            text
-        )[0]
-        image_draw.text(
-            xy=(center, calc_y_axis(bounding_bottoms, i)),
-            text=text,
-            fill=font_color,
-            font=image_font,
-            anchor="mm",
-        )
-    resize_base = 128
-    image = image.resize((resize_base, resize_base))
-    image.save(fp=file_name)
-
-
-def calc_font_size(
-        base_size,
-        bottom,
-        font_path,
-        image_font,
-        right,
-        font_size,
-        text):
-    bounding_box = None
-    while (bounding_box is None) or \
-            (bounding_box[right] > base_size) or \
-            (bounding_box[bottom] > base_size) \
-            and (font_size > 0):
-        image_font = ImageFont.truetype(
-            font=font_path,
-            size=font_size
-        )
-        bounding_box = image_font.getbbox(text=text)
-        font_size -= 1
-    return image_font, bounding_box
-
-
-def calc_y_axis(bounding_boxs, count):
-    # TODO: メソッドへの切り出し
-    # 1個の時は0が1個
-    # 2個の時は0が2個, 1が1個
-    # 3個の時は0が2個, 1が2個, 2が1個
-    # 4個の時は0が2個, 1が2個, 2が2個, 3が1個
-    if count == 1:
-        return (bounding_boxs[0]) / 2
-    elif count == 2:
-        return (bounding_boxs[0] + bounding_boxs[0] +
-                bounding_boxs[1]) / 2
-    elif count == 3:
-        return (bounding_boxs[0] + bounding_boxs[0] +
-                bounding_boxs[1] + bounding_boxs[1] +
-                bounding_boxs[2]) / 2
+    def _calc_y_axis(self, bounding_boxs, count):
+        # TODO: メソッドへの切り出し
+        # 1個の時は0が1個
+        # 2個の時は0が2個, 1が1個
+        # 3個の時は0が2個, 1が2個, 2が1個
+        # 4個の時は0が2個, 1が2個, 2が2個, 3が1個
+        if count == 1:
+            return (bounding_boxs[0]) / 2
+        elif count == 2:
+            return (bounding_boxs[0] + bounding_boxs[0] +
+                    bounding_boxs[1]) / 2
+        elif count == 3:
+            return (bounding_boxs[0] + bounding_boxs[0] +
+                    bounding_boxs[1] + bounding_boxs[1] +
+                    bounding_boxs[2]) / 2
 
 
 if __name__ == '__main__':
@@ -148,5 +137,7 @@ if __name__ == '__main__':
         "Scrap\nBox"
     ]
     for input_text in input_texts:
-        main(input_text)
-        # auto_font_size_change(input_text)
+        # main(input_text)
+        make_slack_emoji = MakeSlackEmoji(input_text)
+        make_slack_emoji.main()
+        make_slack_emoji.auto_font_size_change()
