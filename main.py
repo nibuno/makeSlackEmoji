bd622c91a93b3aec98b1c4df9c583d71c8e6bfe8
Yumihiki <nibu10.majik@gmail.com>
2022-11-23 14:53:20 +0900

    不要な変数の修正、メソッドの位置修正

--- main.py
+++ main.py
@@ -44,6 +44,58 @@ def main(text, font_color="#000000"):
     image.save(fp=file_name)
 
 
+# todo: 重複ロジックの切り出し
+def auto_font_size_change(texts, font_color="#000000"):
+    file_stem = "_".join(texts.splitlines())
+    file_suffix = ".png"
+    file_name = file_stem + file_suffix
+    background_color = (0, 0, 0, 0)
+    base_size = 128 * 2
+    center = base_size / 2
+    image_font = None
+    font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
+    right = 2
+    bottom = 3
+    bounding_bottoms = []
+    for text in texts.splitlines():
+        bounding_box = calc_font_size(
+            base_size,
+            bottom,
+            font_path,
+            image_font,
+            right,
+            base_size,
+            text
+        )[1]
+        bounding_bottoms.append(bounding_box[bottom])
+    image = Image.new(
+        mode="RGBA",
+        size=(base_size, sum(bounding_bottoms)),
+        color=background_color
+    )
+    image_draw = ImageDraw.Draw(im=image)
+    for i, text in enumerate(texts.splitlines(), start=1):
+        image_font = calc_font_size(
+            base_size,
+            bottom,
+            font_path,
+            image_font,
+            right,
+            base_size,
+            text
+        )[0]
+        image_draw.text(
+            xy=(center, calc_y_axis(bounding_bottoms, i)),
+            text=text,
+            fill=font_color,
+            font=image_font,
+            anchor="mm",
+        )
+    resize_base = 128
+    image = image.resize((resize_base, resize_base))
+    image.save(fp=file_name)
+
+
 def calc_font_size(
         base_size,
         bottom,
@@ -66,75 +118,21 @@ def calc_font_size(
     return image_font, bounding_box
 
 
-# todo: 重複ロジックの切り出し
-def auto_font_size_change(texts, font_color="#000000"):
-    file_stem = "_".join(texts.splitlines())
-    file_suffix = ".png"
-    file_name = file_stem + file_suffix
-    background_color = (0, 0, 0, 0)
-    base_size = 128 * 2
-    center = base_size / 2
-    image_font = None
-    font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
-    right = 2
-    bottom = 3
-    bounding_bottoms = []
-    for text in texts.splitlines():
-        image_font, bounding_box = calc_font_size(
-            base_size,
-            bottom,
-            font_path,
-            image_font,
-            right,
-            base_size,
-            text
-        )
-        bounding_bottoms.append(bounding_box[bottom])
-    image = Image.new(
-        mode="RGBA",
-        size=(base_size, sum(bounding_bottoms)),
-        color=background_color
-    )
-    image_draw = ImageDraw.Draw(im=image)
-    y = None
-    for i, text in enumerate(texts.splitlines(), start=1):
-        image_font, bounding_box = calc_font_size(
-            base_size,
-            bottom,
-            font_path,
-            image_font,
-            right,
-            base_size,
-            text
-        )
-        image_draw.text(
-            xy=(center, calc_y_axis(bounding_bottoms, i, y)),
-            text=text,
-            fill=font_color,
-            font=image_font,
-            anchor="mm",
-        )
-    resize_base = 128
-    image = image.resize((resize_base, resize_base))
-    image.save(fp=file_name)
-
-
-def calc_y_axis(bounding_boxs, i, y):
+def calc_y_axis(bounding_boxs, count):
     # TODO: メソッドへの切り出し
     # 1個の時は0が1個
     # 2個の時は0が2個, 1が1個
     # 3個の時は0が2個, 1が2個, 2が1個
     # 4個の時は0が2個, 1が2個, 2が2個, 3が1個
-    if i == 1:
-        y = (bounding_boxs[0]) / 2
-    elif i == 2:
-        y = (bounding_boxs[0] + bounding_boxs[0] +
-             bounding_boxs[1]) / 2
-    elif i == 3:
-        y = (bounding_boxs[0] + bounding_boxs[0] +
-             bounding_boxs[1] + bounding_boxs[1] +
-             bounding_boxs[2]) / 2
-    return y
+    if count == 1:
+        return (bounding_boxs[0]) / 2
+    elif count == 2:
+        return (bounding_boxs[0] + bounding_boxs[0] +
+                bounding_boxs[1]) / 2
+    elif count == 3:
+        return (bounding_boxs[0] + bounding_boxs[0] +
+                bounding_boxs[1] + bounding_boxs[1] +
+                bounding_boxs[2]) / 2
 
 
 if __name__ == '__main__':
