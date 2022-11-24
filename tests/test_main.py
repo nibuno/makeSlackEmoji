849cc0ab78cdea3fdb6c06e48aaee5abbce6471e
Yumihiki <nibu10.majik@gmail.com>
2022-11-24 22:20:03 +0900

    フォントサイズの計算ロジックのテストコードを追加

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,23 @@ from main import MakeSlackEmoji
 
 class TestMakeSlackEmoji:
     def test__calc_font_size(self):
-        make_slack_emoji = MakeSlackEmoji("Test")
+        make_slack_emoji = MakeSlackEmoji("弓")
+        make_slack_emoji.base_size = 128
+        assert make_slack_emoji._calc_font_size(256, "弓")[1] == (0, 30, 108, 128)
+        make_slack_emoji = MakeSlackEmoji("弓")
         make_slack_emoji.base_size = 128 * 2
         assert make_slack_emoji._calc_font_size(256, "弓")[1] == (0, 60, 217, 256)
+
+        make_slack_emoji = MakeSlackEmoji("hello")
+        make_slack_emoji.base_size = 128
+        assert make_slack_emoji._calc_font_size(256, "hello")[1] == (0, 16, 127, 55)
+        make_slack_emoji = MakeSlackEmoji("hello")
+        make_slack_emoji.base_size = 128 * 2
+        assert make_slack_emoji._calc_font_size(256, "hello")[1] == (0, 33, 256, 109)
+
+        make_slack_emoji = MakeSlackEmoji("Yumihiki")
+        make_slack_emoji.base_size = 128
+        assert make_slack_emoji._calc_font_size(256, "Yumihiki")[1] == (0, 10, 127, 32)
+        make_slack_emoji = MakeSlackEmoji("Yumihiki")
+        make_slack_emoji.base_size = 128 * 2
+        assert make_slack_emoji._calc_font_size(256, "Yumihiki")[1] == (0, 19, 254, 62)
