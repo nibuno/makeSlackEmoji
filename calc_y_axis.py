ad6743bbbdb8980b74b27710ab9d7b4ac1ed3e4f
Yumihiki <nibu10.majik@gmail.com>
2022-11-24 21:08:43 +0900

    Y軸の描画位置計算ロジックの修正 ・docstringの修正 ・型ヒントの追加

--- calc_y_axis.py
+++ calc_y_axis.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def calc_y_axis(bounding_boxs, count) -> int:
+def calc_y_axis(bounding_bottoms: list, count: int) -> int:
     """Y軸の描画位置を取得する
 
     境界ボックスの位置を利用して各出力位置の中心を取得する
@@ -10,14 +10,14 @@ def calc_y_axis(bounding_boxs, count) -> int:
     count: 2 bounding_boxs[0] + (bounding_boxs[1] / 2)
     count: 3 bounding_boxs[0] + bounding_boxs[1] + (bounding_boxs[2] / 2)
 
-    :param bounding_boxs: 境界ボックス
+    :param bounding_bottoms: 境界ボックスの下部分のリスト
     :param count: カウント
     :return: 描画位置
     """
-    results = []
+    results: list = []
     for i in range(count):
         if i == count - 1:
-            results.append(bounding_boxs[i] / 2)
+            results.append(bounding_bottoms[i] / 2)
         else:
-            results.append(bounding_boxs[i])
+            results.append(bounding_bottoms[i])
     return int(sum(results))
