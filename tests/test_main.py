c880feb887a880590c23aa0a4ad3828fb38fa780
Yumihiki <nibu10.majik@gmail.com>
2022-11-26 08:21:31 +0900

    テストコードの修正 1行が長かったので改行と、テスト結果がGitHubActions上とローカル環境で異なるので別の値を付与

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,13 +4,16 @@ from main import MakeSlackEmoji
 class TestMakeSlackEmoji:
     def test__calc_font_size(self):
         make_slack_emoji = MakeSlackEmoji("弓")
-        make_slack_emoji.base_size = 128
-        assert make_slack_emoji._calc_font_size(256, "弓")[1] == (0, 30, 108, 128)
+        make_slack_emoji.base_size = 100
+        assert make_slack_emoji._calc_font_size(100, "弓")[1] \
+               == (0, 23, 84, 100)
 
         make_slack_emoji = MakeSlackEmoji("hello")
-        make_slack_emoji.base_size = 128
-        assert make_slack_emoji._calc_font_size(256, "hello")[1] == (0, 16, 127, 55)
+        make_slack_emoji.base_size = 100
+        assert make_slack_emoji._calc_font_size(100, "hello")[1] \
+               == (0, 12, 99, 43)
 
         make_slack_emoji = MakeSlackEmoji("Yumihiki")
-        make_slack_emoji.base_size = 128
-        assert make_slack_emoji._calc_font_size(256, "Yumihiki")[1] == (0, 10, 127, 32)
+        make_slack_emoji.base_size = 100
+        assert make_slack_emoji._calc_font_size(100, "Yumihiki")[1] \
+               == (0, 7, 99, 25)
