ad3dd269b05c18bd96c245ba134a3eaddadc9518
Yumihiki <nibu10.majik@gmail.com>
2023-03-19 20:18:44 +0900

    dataclassを利用して可読性向上

--- src/entity/emoji.py
+++ src/entity/emoji.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Tuple
 
 
+@dataclass
 class Emoji:
-    def __init__(
-            self,
-            text: str,
-            extension: str = ".png",
-            background_color: Tuple[int, int, int, int] = (0, 0, 0, 0),
-            font: str = "fonts/rounded-mplus-20150529/rounded-mplus-1c-black.ttf",
-            font_color: str = "#000000",
-            base_size: int = 128
-    ):
-        self.text: str = text
-        self.file_extension: str = extension
+    text: str
+    file_extension: str = ".png"
+    background_color: Tuple[int, int, int, int] = (0, 0, 0, 0)
+    font_color: str = "#000000"
+    base_size: int = 128
 
-        self.background_color: Tuple[int, int, int, int] = background_color
+    def __post_init__(self):
+        font = "fonts/rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
         project_root = Path(__file__).parent.parent.parent
         self.font: str = str(project_root / font)
-        self.font_color: str = font_color
-        self.base_size: int = base_size
