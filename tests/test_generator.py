1b04620a71c9f2ceccd17646b512a295a676fda2
Yumihiki <nibu10.majik@gmail.com>
2023-03-19 16:49:36 +0900

    テストが落ちていたため修正

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,10 +1,14 @@
-from src.generator import StandardGeneratorImpl
+from src.entity.emoji import Emoji
 from src.find_best_font_and_box import find_best_font_and_box
+from src.generator import StandardGeneratorImpl
+from src.use_case.emoji_use_case import EmojiUseCase
 
 
 class TestStandardGeneratorImpl:
     def test__calc_font_size(self):
-        generator = StandardGeneratorImpl("弓")
+        emoji = Emoji("弓")
+        emoji_use_case = EmojiUseCase(emoji)
+        generator = StandardGeneratorImpl(emoji, emoji_use_case)
         generator.emoji.base_size = 100
         assert find_best_font_and_box(
             generator.emoji_use_case.get_split_size(),
