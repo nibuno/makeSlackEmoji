6f1d5da2c79edd7622dbdffaac4c3382b5fe3bda
Yumihiki <nibu10.majik@gmail.com>
2023-03-19 10:21:16 +0900

    TypeHintの導入と一部コードの修正

--- main.py
+++ main.py
@@ -1,88 +1,81 @@
 # -*- coding: utf-8 -*-
+from typing import Tuple, Optional, List
+
 from PIL import Image, ImageDraw, ImageFont
 from calc_y_axis import calc_y_axis
 
 
 class MakeSlackEmoji:
     def __init__(self, text):
-        self.text = text
-        self.file_stem = "_".join(self.text.splitlines())
-        self.file_suffix = ".png"
-        self.file_name = self.file_stem + self.file_suffix
-        self.save_file_path = "save/" + self.file_name
-        self.background_color = (0, 0, 0, 0)
-        self.font_path = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
-        self.base_size = 128
-        self.bounding_right_num = 2
-        self.bounding_bottom_num = 3
+        self.text: str = text
+        self.file_stem: str = "_".join(self.text.splitlines())
+        self.file_suffix: str = ".png"
+        self.file_name: str = self.file_stem + self.file_suffix
+        self.save_file_path: str = "save/" + self.file_name
+        self.background_color: Tuple[int, int, int, int] = (0, 0, 0, 0)
+        self.font_path: str = "rounded-mplus-20150529/rounded-mplus-1c-black.ttf"
+        self.font_color: str = "#000000"
+        self.base_size: int = 128
+        self.bounding_right_num: int = 2
+        self.bounding_bottom_num: int = 3
 
-    def judgment(self, auto_font_size_mode):
-        # todo: 判定ロジックを修正したい
-        if auto_font_size_mode:
-            self.auto_font_size_change()
-        else:
-            self.main()
-
-    def main(self, font_color="#000000"):
-        image = Image.new(
+    def create(self):
+        image: Image = Image.new(
             mode="RGBA",
             size=(self.base_size, self.base_size),
             color=self.background_color
         )
-        image_draw = ImageDraw.Draw(im=image)
-        count = 1
+        image_draw: ImageDraw = ImageDraw.Draw(im=image)
+        count: int = 1
         for text in self.text.splitlines():
-            image_font = self._calc_font_size(
+            image_font: ImageFont = self._calc_font_size(
                 self._get_split_size(),
                 text
             )[0]
             image_draw.text(
                 xy=(self._get_center(), (self._get_split_size() / 2) * count),
                 text=text,
-                fill=font_color,
+                fill=self.font_color,
                 font=image_font,
                 anchor="mm",
             )
             count += 2
         image.save(fp=self.save_file_path)
 
-    def auto_font_size_change(self, font_color="#000000"):
-        resize = self.base_size
-        self.base_size = 128 * 2
-        bounding_bottoms = []
+    def auto_font_size_change(self):
+        resize: int = self.base_size
+        self.base_size: int = 128 * 2
+        bounding_bottoms: List = []
         for text in self.text.splitlines():
             bounding_box = self._calc_font_size(
                 self.base_size,
                 text
             )[1]
             bounding_bottoms.append(bounding_box[self.bounding_bottom_num])
-        image = Image.new(
+        image: Image = Image.new(
             mode="RGBA",
             size=(self.base_size, sum(bounding_bottoms)),
             color=self.background_color
         )
-        image_draw = ImageDraw.Draw(im=image)
+        image_draw: ImageDraw = ImageDraw.Draw(im=image)
         for i, text in enumerate(self.text.splitlines(), start=1):
-            image_font = self._calc_font_size(
+            image_font: Tuple[int, int, int, int] = self._calc_font_size(
                 self.base_size,
                 text
             )[0]
             image_draw.text(
                 xy=(self._get_center(), calc_y_axis(bounding_bottoms, i)),
                 text=text,
-                fill=font_color,
+                fill=self.font_color,
                 font=image_font,
                 anchor="mm",
             )
-        image = image.resize((resize, resize))
+        image: Image = image.resize((resize, resize))
         image.save(fp=self.save_file_path)
 
-    def _calc_font_size(
-            self,
-            font_size,
-            text):
-        image_font = None
-        bounding_box = None
+    def _calc_font_size(self, font_size: int, text: str):
+        image_font: Optional[ImageFont] = None
+        bounding_box: Optional[Tuple[int, int, int, int]] = None
         while (bounding_box is None) or \
                 (bounding_box[self.bounding_right_num] > self.base_size) or \
                 (bounding_box[self.bounding_bottom_num] > self.base_size) \
@@ -95,13 +88,21 @@ class MakeSlackEmoji:
             font_size -= 1
         return image_font, bounding_box
 
-    def _get_split_size(self):
+    def _get_split_size(self) -> int:
         return int(
             self.base_size / len(self.text.splitlines())
         )
 
-    def _get_center(self):
+    def _get_center(self) -> float:
         return self.base_size / 2
+
+
+    def judgment(self, auto_font_size_mode):
+        # todo: 判定ロジックを修正したい
+        if auto_font_size_mode:
+            self.auto_font_size_change()
+        else:
+            self.create()
 
 
 if __name__ == '__main__':
