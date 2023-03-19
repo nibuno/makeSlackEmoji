b81fdde7e1dfec8b0d3183f7ae9626c6a28c83a9
Yumihiki <nibu10.majik@gmail.com>
2023-03-19 12:45:55 +0900

    １つのクラスで２つのメソッドにしていたが、 似て非なる機能だと考え、別々のクラスに分割

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-from typing import Tuple, Optional, List
-
 from PIL import Image, ImageDraw, ImageFont
 from calc_y_axis import calc_y_axis
-
 from emoji import Emoji
+from src.interface.image_generator import ImageGenerator
+from typing import Tuple, Optional, List
 
 
-class MakeSlackEmoji:
+class StandardGeneratorImpl(ImageGenerator):
     def __init__(self, text: str):
         self.emoji: Emoji = Emoji(text)
         self.bounding_right_num: int = 2
         self.bounding_bottom_num: int = 3
 
-    def create(self):
+    def generate(self):
         image: Image = Image.new(
             mode="RGBA",
             size=(self.emoji.base_size, self.emoji.base_size),
@@ -41,36 +40,62 @@ class MakeSlackEmoji:
             count += 2
         image.save(fp=self.emoji.get_save_file_path())
 
-    def auto_font_size_change(self):
-        resize: int = self.base_size
-        self.base_size: int = 128 * 2
+    def _calc_font_size(self, font_size: int, text: str, font: str, base_size: int):
+        image_font: Optional[ImageFont] = None
+        bounding_box: Optional[Tuple[int, int, int, int]] = None
+        while (bounding_box is None) or \
+                (bounding_box[self.bounding_right_num] > base_size) or \
+                (bounding_box[self.bounding_bottom_num] > base_size) \
+                and (font_size > 0):
+            image_font = ImageFont.truetype(
+                font=font,
+                size=font_size
+            )
+            bounding_box = image_font.getbbox(text=text)
+            font_size -= 1
+        return image_font, bounding_box
+
+
+class AutoFontSizeChangeGeneratorImpl(ImageGenerator):
+    def __init__(self, text: str):
+        self.emoji: Emoji = Emoji(text)
+        self.bounding_right_num: int = 2
+        self.bounding_bottom_num: int = 3
+
+    def generate(self):
+        resize: int = self.emoji.base_size
+        self.emoji.base_size = 128 * 2
         bounding_bottoms: List = []
-        for text in self.text.splitlines():
+        for text in self.emoji.text.splitlines():
             bounding_box = self._calc_font_size(
-                self.base_size,
-                text
+                self.emoji.get_split_size(),
+                text,
+                self.emoji.font,
+                self.emoji.base_size
             )[1]
             bounding_bottoms.append(bounding_box[self.bounding_bottom_num])
         image: Image = Image.new(
             mode="RGBA",
-            size=(self.base_size, sum(bounding_bottoms)),
-            color=self.background_color
+            size=(self.emoji.base_size, sum(bounding_bottoms)),
+            color=self.emoji.background_color
         )
         image_draw: ImageDraw = ImageDraw.Draw(im=image)
-        for i, text in enumerate(self.text.splitlines(), start=1):
+        for i, text in enumerate(self.emoji.text.splitlines(), start=1):
             image_font: Tuple[int, int, int, int] = self._calc_font_size(
-                self.base_size,
-                text
+                self.emoji.get_split_size(),
+                text,
+                self.emoji.font,
+                self.emoji.base_size
             )[0]
             image_draw.text(
-                xy=(self._get_center(), calc_y_axis(bounding_bottoms, i)),
+                xy=(self.emoji.get_center(), calc_y_axis(bounding_bottoms, i)),
                 text=text,
-                fill=self.font_color,
+                fill=self.emoji.font_color,
                 font=image_font,
                 anchor="mm",
             )
         image: Image = image.resize((resize, resize))
-        image.save(fp=self.save_file_path)
+        image.save(fp=self.emoji.get_save_file_path())
 
     def _calc_font_size(self, font_size: int, text: str, font: str, base_size: int):
         image_font: Optional[ImageFont] = None
@@ -87,13 +112,6 @@ class MakeSlackEmoji:
             font_size -= 1
         return image_font, bounding_box
 
-    def judgment(self, auto_font_size_mode):
-        # todo: 判定ロジックを修正したい
-        if auto_font_size_mode:
-            self.auto_font_size_change()
-        else:
-            self.create()
-
 
 if __name__ == '__main__':
     input_texts = [
@@ -109,5 +127,8 @@ if __name__ == '__main__':
     ]
     auto_font_size = False
     for input_text in input_texts:
-        make_slack_emoji = MakeSlackEmoji(input_text)
-        make_slack_emoji.judgment(auto_font_size)
+        if auto_font_size:
+            generator = AutoFontSizeChangeGeneratorImpl(input_text)
+        else:
+            generator = StandardGeneratorImpl(input_text)
+        generator.generate()
