5c1f0d5118b389d8e4023e20b24c2a63f476b3ed
Yumihiki <nibu10.majik@gmail.com>
2023-03-19 13:36:34 +0900

    GitHubActions上でも動作するように修正

--- src/emoji.py
+++ src/emoji.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from typing import Tuple
+from pathlib import Path
 
 
 class Emoji:
@@ -8,7 +9,7 @@ class Emoji:
             text: str,
             extension: str = ".png",
             background_color: Tuple[int, int, int, int] = (0, 0, 0, 0),
-            font: str = "../fonts/rounded-mplus-20150529/rounded-mplus-1c-black.ttf",
+            font: str = "fonts/rounded-mplus-20150529/rounded-mplus-1c-black.ttf",
             font_color: str = "#000000",
             base_size: int = 128
     ):
@@ -16,7 +17,8 @@ class Emoji:
         self.file_extension: str = extension
 
         self.background_color: Tuple[int, int, int, int] = background_color
-        self.font: str = font
+        project_root = Path(__file__).resolve().parent
+        self.font: str = str(project_root / font)
         self.font_color: str = font_color
         self.base_size: int = base_size
 
