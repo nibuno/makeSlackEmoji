79d1f096f9c25a3fd86b07a67e850280419ee613
Yumihiki <nibu10.majik@gmail.com>
2023-03-19 15:09:20 +0900

    テストコードの修正漏れ対応

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,11 +1,12 @@
 from src.generator import StandardGeneratorImpl
+from src.find_best_font_and_box import find_best_font_and_box
 
 
 class TestStandardGeneratorImpl:
     def test__calc_font_size(self):
         generator = StandardGeneratorImpl("弓")
         generator.emoji.base_size = 100
-        assert _calc_font_size(
+        assert find_best_font_and_box(
             generator.emoji_use_case.get_split_size(),
             generator.emoji.text,
             generator.emoji.font,
